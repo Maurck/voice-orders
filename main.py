9eaa7526bc81a8c440724f27ff3630c4851658ac
Sebastian Vargas <vargaspizango1@gmail.com>
2022-08-15 15:07:11 -0500

    feat: add sections in main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from voice_to_text import *
 from record_voice import *
 
+
+# Transcription section
 seconds = 6
 
 record_voice(seconds)
@@ -9,3 +11,5 @@ filename = "audio.m4a"
 audio_url = upload(filename)
 
 save_transcript(audio_url, 'audio_text', seconds)
+
+# Model section
